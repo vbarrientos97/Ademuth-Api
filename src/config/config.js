00615628295948c62b8feb9512fe22886f83412d
Verónica Barrientos Alfaro <vbarrientos1997@gmail.com>
2023-09-13 15:56:15 -0600

    starting workflow with railway

--- src/config/config.js
+++ src/config/config.js
@@ -3,11 +3,11 @@ require("dotenv").config();
 const config = {
   env: process.env.NODE_ENV || "dev",
   port: process.env.PORT || 8000,
-  dbUser: process.env.DB_USER,
-  dbPassword: process.env.DB_PASSWORD,
-  dbHost: process.env.DB_HOST,
-  dbName: process.env.DB_NAME,
-  dbPort: process.env.DB_PORT,
+  dbUser: process.env.DB_USER || "postgres",
+  dbPassword: process.env.DB_PASSWORD || "admin",
+  dbHost: process.env.DB_HOST || "localhost",
+  dbName: process.env.DB_NAME || "verodb",
+  dbPort: process.env.DB_PORT || 5432,
 };
 
 module.exports = { config };
