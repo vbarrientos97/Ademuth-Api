3c78a83632a7840a333ee279fd16d51f30d180fb
Verónica Barrientos Alfaro <vbarrientos1997@gmail.com>
2023-09-13 19:20:59 -0600

    updating files to deploy

--- src/config/config.js
+++ src/config/config.js
@@ -7,7 +7,7 @@ const config = {
   dbPassword: process.env.DB_PASSWORD || "admin",
   dbHost: process.env.DB_HOST || "localhost",
   dbName: process.env.DB_NAME || "verodb",
-  dbPort: process.env.DB_PORT,
+  dbPort: process.env.DB_PORT || 5432,
 };
 
 module.exports = { config };
