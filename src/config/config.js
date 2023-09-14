d86272be0a4aa316fc59942e00f43a14d91f4c2a
Verónica Barrientos Alfaro <vbarrientos1997@gmail.com>
2023-09-13 18:24:24 -0600

    updating config.js

--- src/config/config.js
+++ src/config/config.js
@@ -1,6 +1,7 @@
 require("dotenv").config();
 
 const config = {
+  env: process.env.NODE_ENV || "dev",
   port: process.env.PORT || 8000,
   dbUser: process.env.DB_USER || "postgres",
   dbPassword: process.env.DB_PASSWORD || "admin",
